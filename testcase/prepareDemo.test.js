8850a6f05989ea2bab9d00b3234adcbefffd354c
fonnNutchthasasi <nutchthasasi.t@gmail.com>
2025-03-17 12:50:28 +0700

    prepare test code comment

--- testcase/prepareDemo.test.js
+++ testcase/prepareDemo.test.js
@@ -8,14 +8,14 @@ test.describe('senestia', () => {
     test.beforeAll(async ({ browser }) => {
         page = await browser.newPage(); 
         const loginPage = new LoginPage(page);
-        const url = 'https://www.facebook.com';
+        const url = 'https://www.google.com';
         const username = 'smart_line_sincere@hotmail.com';
         const password = 'pooh0821'
 
         await page.goto(url);
-        await loginPage.inputUsername(username);
-        await loginPage.inputPassword(password);
-        await loginPage.clickLoginButton();
+        // await loginPage.inputUsername(username);
+        // await loginPage.inputPassword(password);
+        // await loginPage.clickLoginButton();
     });
 
     test.afterAll(async () => {
@@ -25,6 +25,7 @@ test.describe('senestia', () => {
 
     test('test login facebook', async () => {
         const commonLib = new CommonLibrary(page);
-        await page.waitForSelector('//div[@aria-label="เมนู"]', { state: 'visible' });
+        //await page.waitForSelector('//div[@aria-label="เมนู"]', { state: 'visible' });
+        console.log("Test ผ่านไหมนะ")
     });
 });
